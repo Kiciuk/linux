41a814c596d3902606c837812059f863d116c30d
Junak <junak.pub@gmail.com>
2020-09-10 19:33:09 +0300

    video: backlight: qcom-wled: bug fixes

--- drivers/video/backlight/qcom-wled.c
+++ drivers/video/backlight/qcom-wled.c
@@ -936,7 +936,7 @@ static const struct wled_config wled3_config_defaults = {
 	.boost_i_limit = 3,
 	.string_i_limit = 20,
 	.ovp = 2,
-	.num_strings = 3,
+	.num_strings = 3 - 1,
 	.switch_freq = 5,
 	.cs_out_en = false,
 	.ext_gen = false,
@@ -1067,7 +1067,7 @@ static const struct wled_config wled4_config_defaults = {
 	.boost_i_limit = 4,
 	.string_i_limit = 10,
 	.ovp = 1,
-	.num_strings = 4,
+	.num_strings = 4 - 1,
 	.switch_freq = 11,
 	.cabc = false,
 	.external_pfet = false,
@@ -1178,7 +1178,7 @@ static const struct wled_config wled5_config_defaults = {
 	.boost_i_limit = 5,
 	.string_i_limit = 10,
 	.ovp = 4,
-	.num_strings = 4,
+	.num_strings = 4 - 1,
 	.switch_freq = 11,
 	.mod_sel = 0,
 	.cabc_sel = 0,
@@ -1532,7 +1532,7 @@ static int wled_configure(struct wled *wled)
 		of_property_read_u32_array(dev->of_node,
 						"qcom,enabled-strings",
 						wled->cfg.enabled_strings,
-						sizeof(u32));
+						string_len);
 
 	return 0;
 }
