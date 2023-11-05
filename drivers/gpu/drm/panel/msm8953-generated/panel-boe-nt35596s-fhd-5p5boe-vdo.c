c1db83ee5a8da6a6d1408f13f0a96c7435654b14
Adam Skladowski <a39.skl@gmail.com>
2023-11-05 20:06:21 +0100

    s2 panel:prepare first

--- drivers/gpu/drm/panel/msm8953-generated/panel-boe-nt35596s-fhd-5p5boe-vdo.c
+++ drivers/gpu/drm/panel/msm8953-generated/panel-boe-nt35596s-fhd-5p5boe-vdo.c
@@ -1010,7 +1010,7 @@ static int boe_nt35596s_5p5boe_vdo_probe(struct mipi_dsi_device *dsi)
 
 	drm_panel_init(&ctx->panel, dev, &boe_nt35596s_5p5boe_vdo_panel_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
-
+        ctx->panel.prepare_prev_first = true;
 	ret = drm_panel_of_backlight(&ctx->panel);
 	if (ret)
 		return dev_err_probe(dev, ret, "Failed to get backlight\n");
