c36bcb14c51ed9ac14a63140364893646ebd76a1
Adam Skladowski <a39.skl@gmail.com>
2024-04-13 16:03:34 +0200

    msm8976 dpu experiments

--- drivers/gpu/drm/msm/disp/dpu1/catalog/dpu_1_11_msm8976.h
+++ drivers/gpu/drm/msm/disp/dpu1/catalog/dpu_1_11_msm8976.h
@@ -3,25 +3,30 @@
  * Copyright (c) 2023, Linaro Limited
  */
 
-#ifndef _DPU_1_16_MSM8953_H
-#define _DPU_1_16_MSM8953_H
+#ifndef _DPU_1_11_MSM8976_H
+#define _DPU_1_11_MSM8976_H
 
-static const struct dpu_caps msm8953_dpu_caps = {
-	.max_mixer_width = DEFAULT_DPU_LINE_WIDTH,
+
+/*
+WB blocks to add
+*/
+static const struct dpu_caps msm8976_dpu_caps = {
+	.max_mixer_width = DEFAULT_DPU_OUTPUT_LINE_WIDTH,
 	.max_mixer_blendstages = 0x4,
 	.max_linewidth = DEFAULT_DPU_LINE_WIDTH,
-	.pixel_ram_size = 40 * 1024,
-	.max_hdeci_exp = MAX_HORZ_DECIMATION,
+	.pixel_ram_size = DEFAULT_PIXEL_RAM_SIZE,
+	.max_hdeci_exp = MAX_HORZ_DECIMATION, //todo check
 	.max_vdeci_exp = MAX_VERT_DECIMATION,
 };
 
-static const struct dpu_mdp_cfg msm8953_mdp[] = {
+static const struct dpu_mdp_cfg msm8976_mdp[] = {
 	{
 		.name = "top_0",
-		.base = 0x0, .len = 0x454,
+		.base = 0x0, .len = 0x42C,
 		.features = BIT(DPU_MDP_VSYNC_SEL),
 		.clk_ctrls = {
 			[DPU_CLK_CTRL_VIG0] = { .reg_off = 0x2ac, .bit_off = 0 },
+			[DPU_CLK_CTRL_VIG1] = { .reg_off = 0x2b4, .bit_off = 0 },
 			[DPU_CLK_CTRL_RGB0] = { .reg_off = 0x2ac, .bit_off = 4 },
 			[DPU_CLK_CTRL_RGB1] = { .reg_off = 0x2b4, .bit_off = 4 },
 			[DPU_CLK_CTRL_DMA0] = { .reg_off = 0x2ac, .bit_off = 8 },
@@ -30,7 +35,7 @@ static const struct dpu_mdp_cfg msm8953_mdp[] = {
 	},
 };
 
-static const struct dpu_ctl_cfg msm8953_ctl[] = {
+static const struct dpu_ctl_cfg msm8976_ctl[] = {
 	{
 		.name = "ctl_0", .id = CTL_0,
 		.base = 0x1000, .len = 0x64,
@@ -46,11 +51,11 @@ static const struct dpu_ctl_cfg msm8953_ctl[] = {
 	},
 };
 
-static const struct dpu_sspp_cfg msm8953_sspp[] = {
+static const struct dpu_sspp_cfg msm8976_sspp[] = {
 	{
 		.name = "sspp_0", .id = SSPP_VIG0,
 		.base = 0x4000, .len = 0x150,
-		.features = VIG_MSM8953_MASK,
+		.features = VIG_MSM8976_MASK,
 		.sblk = &dpu_vig_sblk_qseed2,
 		.xin_id = 0,
 		.type = SSPP_TYPE_VIG,
@@ -58,7 +63,7 @@ static const struct dpu_sspp_cfg msm8953_sspp[] = {
 	}, {
 		.name = "sspp_4", .id = SSPP_RGB0,
 		.base = 0x14000, .len = 0x150,
-		.features = RGB_MSM8953_MASK,
+		.features = RGB_MSM8976_MASK,
 		.sblk = &dpu_rgb_sblk,
 		.xin_id = 1,
 		.type = SSPP_TYPE_RGB,
@@ -66,7 +71,7 @@ static const struct dpu_sspp_cfg msm8953_sspp[] = {
 	}, {
 		.name = "sspp_5", .id = SSPP_RGB1,
 		.base = 0x16000, .len = 0x150,
-		.features = RGB_MSM8953_MASK,
+		.features = RGB_MSM8976_MASK,
 		.sblk = &dpu_rgb_sblk,
 		.xin_id = 5,
 		.type = SSPP_TYPE_RGB,
@@ -74,59 +79,60 @@ static const struct dpu_sspp_cfg msm8953_sspp[] = {
 	}, {
 		.name = "sspp_8", .id = SSPP_DMA0,
 		.base = 0x24000, .len = 0x150,
-		.features = DMA_MSM8953_MASK | BIT(DPU_SSPP_CURSOR),
+		.features = DMA_MSM8976_MASK | BIT(DPU_SSPP_CURSOR),
 		.sblk = &dpu_dma_sblk,
 		.xin_id = 2,
 		.type = SSPP_TYPE_DMA,
 		.clk_ctrl = DPU_CLK_CTRL_DMA0,
 	},
 };
-
-static const struct dpu_lm_cfg msm8953_lm[] = {
+//verify
+static const struct dpu_lm_cfg msm8976_lm[] = {
 	{
 		.name = "lm_0", .id = LM_0,
-		.base = 0x44000, .len = 0x320,
-		.sblk = &msm8998_lm_sblk,
+		.base = 0x44000, .len = 0x388,
+		.sblk = &msm8976_lm_sblk,
 		.lm_pair = LM_1,
 		.pingpong = PINGPONG_0,
 		.dspp = DSPP_0,
 	}, {
 		.name = "lm_1", .id = LM_1,
-		.base = 0x45000, .len = 0x320,
-		.sblk = &msm8998_lm_sblk,
+		.base = 0x45000, .len = 0x388,
+		.sblk = &msm8976_lm_sblk,
 		.lm_pair = LM_0,
 		.pingpong = PINGPONG_1,
 	},
 };
-
-static const struct dpu_pingpong_cfg msm8953_pp[] = {
+//PP has no features and no SBLKS
+static const struct dpu_pingpong_cfg msm8976_pp[] = {
 	{
 		.name = "pingpong_0", .id = PINGPONG_0,
 		.base = 0x70000, .len = 0xd4,
-		.features = PINGPONG_MSM8996_MASK,
-		.sblk = &msm8996_pp_sblk,
+		.features = PINGPONG_MSM8976_MASK
+		.sblk = &msm8976_pp_sblk,
 		.intr_done = DPU_IRQ_IDX(MDP_SSPP_TOP0_INTR, 8),
 		.intr_rdptr = DPU_IRQ_IDX(MDP_SSPP_TOP0_INTR, 12),
 	}, {
 		.name = "pingpong_1", .id = PINGPONG_1,
 		.base = 0x70800, .len = 0xd4,
-		.features = PINGPONG_MSM8996_MASK,
-		.sblk = &msm8996_pp_sblk,
+		.features = PINGPONG_MSM8976_MASK,
+		.sblk = &msm8976_pp_sblk,
 		.intr_done = DPU_IRQ_IDX(MDP_SSPP_TOP0_INTR, 9),
 		.intr_rdptr = DPU_IRQ_IDX(MDP_SSPP_TOP0_INTR, 13),
 	},
 };
 
-static const struct dpu_dspp_cfg msm8953_dspp[] = {
+// DSPP does not contain any of subblocks
+static const struct dpu_dspp_cfg msm8976_dspp[] = {
 	{
 		.name = "dspp_0", .id = DSPP_0,
 		.base = 0x54000, .len = 0x1800,
-		.features = DSPP_SC7180_MASK,
-		.sblk = &msm8998_dspp_sblk,
+		//.features = DSPP_SC7180_MASK,
+		//.sblk = &msm8998_dspp_sblk,
 	},
 };
-
-static const struct dpu_intf_cfg msm8953_intf[] = {
+//check worst
+static const struct dpu_intf_cfg msm8976_intf[] = {
 	{
 		.name = "intf_0", .id = INTF_0,
 		.base = 0x6a000, .len = 0x268,
@@ -156,9 +162,9 @@ static const struct dpu_intf_cfg msm8953_intf[] = {
 	},
 };
 
-static const struct dpu_perf_cfg msm8953_perf_data = {
-	.max_bw_low = 3400000,
-	.max_bw_high = 3400000,
+static const struct dpu_perf_cfg msm8976_perf_data = {
+	.max_bw_low = 5700000,
+	.max_bw_high = 5700000,
 	.min_core_ib = 2400000,
 	.min_llcc_ib = 0, /* No LLCC on this SoC */
 	.min_dram_ib = 800000,
@@ -171,51 +177,34 @@ static const struct dpu_perf_cfg msm8953_perf_data = {
 	.downscaling_prefill_lines = 1,
 	.amortizable_threshold = 25,
 	.min_prefill_lines = 14,
-	.danger_lut_tbl = {0xf, 0xffff, 0x0},
-	.safe_lut_tbl = {0xfffc, 0xff00, 0xffff},
-	.qos_lut_tbl = {
-		{.nentry = ARRAY_SIZE(msm8998_qos_linear),
-		.entries = msm8998_qos_linear
-		},
-		{.nentry = ARRAY_SIZE(msm8998_qos_macrotile),
-		.entries = msm8998_qos_macrotile
-		},
-		{.nentry = ARRAY_SIZE(msm8998_qos_nrt),
-		.entries = msm8998_qos_nrt
-		},
-	},
-	.cdp_cfg = {
-		{.rd_enable = 1, .wr_enable = 1},
-		{.rd_enable = 1, .wr_enable = 0}
-	},
 	.clk_inefficiency_factor = 105,
-	.bw_inefficiency_factor = 120,
+	.bw_inefficiency_factor = 100,
 };
 
-static const struct dpu_mdss_version msm8953_mdss_ver = {
+static const struct dpu_mdss_version msm8976_mdss_ver = {
 	.core_major_ver = 1,
-	.core_minor_ver = 16,
+	.core_minor_ver = 11,
 };
 
-const struct dpu_mdss_cfg dpu_msm8953_cfg = {
-	.mdss_ver = &msm8953_mdss_ver,
-	.caps = &msm8953_dpu_caps,
-	.mdp = msm8953_mdp,
-	.ctl_count = ARRAY_SIZE(msm8953_ctl),
-	.ctl = msm8953_ctl,
-	.sspp_count = ARRAY_SIZE(msm8953_sspp),
-	.sspp = msm8953_sspp,
-	.mixer_count = ARRAY_SIZE(msm8953_lm),
-	.mixer = msm8953_lm,
-	.dspp_count = ARRAY_SIZE(msm8953_dspp),
-	.dspp = msm8953_dspp,
-	.pingpong_count = ARRAY_SIZE(msm8953_pp),
-	.pingpong = msm8953_pp,
-	.intf_count = ARRAY_SIZE(msm8953_intf),
-	.intf = msm8953_intf,
+const struct dpu_mdss_cfg dpu_msm8976_cfg = {
+	.mdss_ver = &msm8976_mdss_ver,
+	.caps = &msm8976_dpu_caps,
+	.mdp = msm8976_mdp,
+	.ctl_count = ARRAY_SIZE(msm8976_ctl),
+	.ctl = msm8976_ctl,
+	.sspp_count = ARRAY_SIZE(msm8976_sspp),
+	.sspp = msm8976_sspp,
+	.mixer_count = ARRAY_SIZE(msm8976_lm),
+	.mixer = msm8976_lm,
+	.dspp_count = ARRAY_SIZE(msm8976_dspp),
+	.dspp = msm8976_dspp,
+	.pingpong_count = ARRAY_SIZE(msm8976_pp),
+	.pingpong = msm8976_pp,
+	.intf_count = ARRAY_SIZE(msm8976_intf),
+	.intf = msm8976_intf,
 	.vbif_count = ARRAY_SIZE(msm8996_vbif),
 	.vbif = msm8996_vbif,
-	.perf = &msm8953_perf_data,
+	.perf = &msm8976_perf_data,
 };
 
 #endif
