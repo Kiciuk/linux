31257e3ef61af237311aa06ce4362653bd910a9a
Adam Skladowski <a39.skl@gmail.com>
2024-08-09 13:56:39 +0200

    crap

--- sound/soc/qcom/qdsp6/q6afe-clocks.c
+++ sound/soc/qcom/qdsp6/q6afe-clocks.c
@@ -11,62 +11,12 @@
 #include "q6dsp-lpass-clocks.h"
 #include "q6afe.h"
 
-struct q6dsp_clk_v1_init {
-	int clk_id;
-	int q6dsp_clk_id;
-	char *name;
-	int rate;
-	int clk_root;
-	int clk_src;
-	int dir;
-	struct q6afe_port *port;
-};
-
 #define Q6AFE_CLK(id) {					\
 		.clk_id	= id,				\
 		.q6dsp_clk_id	= Q6AFE_##id,		\
 		.name = #id,				\
 		.rate = 19200000,			\
 	}
-
-#define Q6AFE_CLK_DIG_V1(id) {					\
-		.clk_id	= id,				\
-		.clk_id	= LPAIF_DIG_CLK,		\
-		.name = #id,				\
-		.port=,					\
-		.clk_src= 0,				\
-		.clk_root= 5,				\
-		.rate = 19200000,			\
-	}
-
-#define Q6AFE_CLK_BIT_V1(id,port) {					\
-		.clk_id	= id,				\
-		.clk_id	= LPAIF_BIT_CLK,		\
-		.name = #id,				\
-		.port=*q6afe_port_get_from_id,					\
-		.clk_src=Q6AFE_LPASS_CLK_SRC_INTERNAL,				\
-		.clk_root=Q6AFE_LPASS_CLK_ROOT_DEFAULT,				\
-		.rate = 19200000,			\
-	}
-	
-#define Q6AFE_CLK_OSR_V1(id) {					\
-		.clk_id	= id,				\
-		.clk_id	= LPAIF_OSR_CLK,		\
-		.name = #id,				\
-		.port=,					\
-		.clk_src=Q6AFE_LPASS_CLK_SRC_INTERNAL,				\
-		.clk_root=Q6AFE_LPASS_CLK_ROOT_DEFAULT,				\
-		.rate = 19200000,			\
-	}
-
-static const struct q6dsp_clk_v1_init q6afe_clks[] = {
-	Q6AFE_CLK_BIT_V1(LPASS_CLK_ID_PRI_MI2S_IBIT, AFE_PORT_ID_PRIMARY_MI2S_RX),
-	Q6AFE_CLK_BIT_V1(LPASS_CLK_ID_SEC_MI2S_IBIT, AFE_PORT_ID_SECONDARY_MI2S_RX),
-	Q6AFE_CLK_BIT_V1(LPASS_CLK_ID_TER_MI2S_IBIT, AFE_PORT_ID_TERTIARY_MI2S_RX),
-	Q6AFE_CLK_BIT_V1(LPASS_CLK_ID_QUAD_MI2S_IBIT, AFE_PORT_ID_QUATERNARY_MI2S_RX),
-	Q6AFE_CLK_BIT_V1(LPASS_CLK_ID_QUI_MI2S_IBIT, AFE_PORT_ID_QUINARY_MI2S_RX),		
-	Q6AFE_CLK_DIG_V1(LPASS_CLK_ID_INTERNAL_DIGITAL_CODEC_CORE,AFE_PORT_ID_PRIMARY_MI2S_RX),
-};
 
 static const struct q6dsp_clk_init q6afe_clks[] = {
 	Q6AFE_CLK(LPASS_CLK_ID_PRI_MI2S_IBIT),
@@ -140,18 +90,12 @@ static const struct q6dsp_clk_init q6afe_clks[] = {
 		       "LPASS_HW_DCODEC"),
 };
 
-static const struct q6dsp_clk_desc q6dsp_clk_q6afe_v2 __maybe_unused = {
+static const struct q6dsp_clk_desc q6dsp_clk_q6afe __maybe_unused = {
 	.clks = q6afe_clks,
 	.num_clks = ARRAY_SIZE(q6afe_clks),
 	.lpass_set_clk = q6afe_set_lpass_clock,
 	.lpass_vote_clk = q6afe_vote_lpass_core_hw,
 	.lpass_unvote_clk = q6afe_unvote_lpass_core_hw,
-};
-
-static const struct q6dsp_clk_desc q6dsp_clk_q6afe_v1 __maybe_unused = {
-	.clks = q6afe_clks_v1,
-	.num_clks = ARRAY_SIZE(q6afe_clks_v1),
-	.lpass_set_clk = q6afe_port_set_sysclk,
 };
 
 #ifdef CONFIG_OF
